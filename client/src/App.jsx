754397552b614da613e2677cba41063b51c73105
Ahmed Ayman <ahmed.ayman.21021999@gmail.com>
2023-12-29 18:36:21 +0200

    add shop routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import AppLayout from "./UI/AppLayout";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Shop from "./UI/shop/Shop";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route element={<AppLayout />}>
           <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
