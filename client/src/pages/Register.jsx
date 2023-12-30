702bfe095f42e29fbb3809a94ff61e0fb3cdd24d
ahmed mosad <awahwa123@gmail.com>
2023-12-31 00:33:58 +0200

    done login

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,43 @@
+import styles from "./Register.module.css";
+import Container from "../UI/container/Container";
+import Loginbuttons from "./components/Loginbuttons";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Container>
+      <h2>sign up</h2>
+      <hr />
+      <div className={styles.main}>
+        <form>
+          <label>Email Address</label>
+          <input placeholder="Please Enter Your Email" />
+          <label>First Name</label>
+          <input placeholder="Please Enter Your First Name" />
+          <label>Last Name</label>
+          <input placeholder="Please Enter Your Last Name" />
+          <label>Password</label>
+          <input placeholder="Please Enter Your Password" />
+        </form>
+        <div>
+          <Loginbuttons />
+        </div>
+      </div>
+      <hr />
+      <div>
+        <div className={styles.Subscribe}>
+          <input type="checkbox" />
+          <span>Subscribe to newsletter</span>
+        </div>
+        <div className={styles.signlink}>
+          <button>Sign up </button>
+          <button className={styles.back}>
+            <Link to="/login">Back to login</Link>
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Register;
