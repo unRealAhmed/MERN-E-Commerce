ac4ebee2e7b9945f236b3b52b5edab6f84739285
Ahmed Sayed <therealahme@gmail.com>
2024-01-03 00:01:30 +0200

    fix delete and update address to match current loggedIn user

--- Server/controllers/address.js
+++ Server/controllers/address.js
@@ -1,6 +1,8 @@
 const Address = require('../models/Address');
 const asyncHandler = require('../utils/asyncHandler');
 const AppError = require('../utils/appErrors');
+const User = require('../models/User');
+
 
 exports.createAddress = asyncHandler(async (req, res, next) => {
   const { address, city, state, country, zipCode } = req.body;
@@ -9,10 +11,22 @@ exports.createAddress = asyncHandler(async (req, res, next) => {
     return next(new AppError('Incomplete address information. All fields are required.', 400));
   }
 
-  const newAddress = await Address.create({ address, city, state, country, zipCode });
+  const newAddress = await Address.create({
+    address,
+    city,
+    state,
+    country,
+    zipCode,
+    user: req.user.id,
+  });
+
+  const user = await User.findById(req.user.id);
+  user.address.push(newAddress);
+  await user.save();
 
   res.status(201).json({ success: true, address: newAddress });
 });
+
 
 exports.getAllAddresses = asyncHandler(async (req, res, next) => {
   const addresses = await Address.find({ user: req.user._id });
@@ -36,24 +50,31 @@ exports.updateAddress = asyncHandler(async (req, res, next) => {
     return next(new AppError('Incomplete address information. All fields are required.', 400));
   }
 
-  const updatedAddress = await Address.findByIdAndUpdate(
-    req.params.id,
+  const user = await User.findById(req.user.id);
+
+  const updatedAddress = await Address.findOneAndUpdate(
+    { _id: req.params.id, user: user._id },
     { address, city, state, country, zipCode },
     { new: true, runValidators: true }
   );
 
   if (!updatedAddress) {
-    return next(new AppError(`Address with ID ${req.params.id} not found`, 404));
+    return next(new AppError(`Address with ID ${req.params.id} not found for the current user`, 404));
   }
 
   res.status(200).json({ success: true, address: updatedAddress });
 });
 
 exports.deleteAddress = asyncHandler(async (req, res, next) => {
-  const address = await Address.findByIdAndDelete(req.params.id);
+  const user = await User.findById(req.user.id);
+
+  const address = await Address.findOneAndDelete({
+    _id: req.params.id,
+    user: user._id,
+  });
 
   if (!address) {
-    return next(new AppError(`Address with ID ${req.params.id} not found`, 404));
+    return next(new AppError(`Address with ID ${req.params.id} not found for the current user`, 404));
   }
 
   res.status(200).json({ success: true, address });
